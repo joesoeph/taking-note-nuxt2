74d3974705514fd771460fdb6fc4e24b3504c6a9
joesoeph <yusuffazeri00@gmail.com>
2022-04-04 15:55:41 +0700

    Adding notes display list

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,47 @@
 <template>
-  <div class="container pt-12 mx-auto">
+  <div class="pt-12">
     <div class="mb-6 text-right">
-      <NuxtLink to="create" class="px-5 py-3 font-bold text-white bg-blue-400 cursor-pointer rounded-xl hover:bg-blue-600">
+      <NuxtLink to="create" class="px-5 py-3 font-bold text-white bg-blue-600 cursor-pointer rounded-xl hover:bg-blue-400">
         + New Note
       </NuxtLink>
     </div>
-    <NoteListPlain
-      title="Contoh judul"
-      created-at="3 months ago"
-      slug="contoh-judul"
-    />
-    <NoteListImage
-      title="Contoh judul"
-      created-at="3 months ago"
-      slug="contoh-judul"
-      background-image="https://picsum.photos/200"
-    />
+    <client-only>
+      <div v-if="$fetchState.pending || $fetchState.error">
+        <div v-for="index in 5" :key="index" class="flex flex-row items-center mb-4">
+          <div class="w-6 h-6 mr-4 bg-gray-200 animate-pulse"></div>
+          <div class="flex flex-row items-center justify-between flex-1 p-6 bg-gray-200 rounded-xl">
+            <div class="w-full animate-pulse">
+              <div class="w-1/2 h-8 mb-2 bg-gray-300"></div>
+              <div class="w-1/4 h-4 bg-gray-300"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <NoteListPlain v-for="(item, index) in notes" :key="index" :background-image="item.image" :slug="item.id" :created-at="humanDate(item.created_at)" :title="item.title"/>
+      </div>
+    </client-only>
   </div>
 </template>
 <script>
+const domain = 'https://620496a1c6d8b20017dc3598.mockapi.io'
 export default {
-    layout: "default",
+  scrollToTop: true,
+  layout: "default",
+  data() {
+    return {
+      notes: []
+    };
+  },
+  async fetch() {
+    const response = await this.$axios.get(`${domain}/notes`)
+    this.notes = response.data
+  },
+  methods: {
+    humanDate(date) {
+      return this.$hDate(date)
+    }
+  },
 }
 </script>
 

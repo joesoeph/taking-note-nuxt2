b649ffd766bc5568518d10c9d8debdfaee2c4c36
joesoeph <yusuffazeri00@gmail.com>
2022-04-04 15:55:51 +0700

    Adding note detail display

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="container pt-12 mx-auto mb-12">
-    <div class="flex flex-col bg-white rounded-b-2xl filter drop-shadow-lg">
-      <div style="background-image: url(https://picsum.photos/200)" class="w-full bg-center bg-no-repeat bg-cover rounded-t-2xl h-96"></div>
-      <div class="p-8">
-        <h1 class="text-4xl font-semibold text-gray-600 ">Some title</h1>
-        <span class="mt-3 text-sm font-light text-gray-600">1 months ago</span>
-        <div class="mt-6 text-gray-600">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus non nisi quos dolor quidem eius consequatur fugiat assumenda, molestias, ipsam error praesentium explicabo ullam ipsa provident eos laboriosam id velit.</p>
+    <client-only>
+      <div v-if="$fetchState.pending || $fetchState.error">Loading...</div>
+      <div v-else class="flex flex-col bg-white rounded-b-2xl filter drop-shadow-lg">
+        <div :style="{'backgroundImage': `url(${note.image})`}" class="w-full bg-center bg-no-repeat bg-cover rounded-t-2xl h-96"></div>
+        <div class="p-8">
+          <h1 class="text-4xl font-semibold text-gray-600 ">{{ note.title }}</h1>
+          <span class="mt-3 text-sm font-light text-gray-600">{{ humanDate(note.created_at) }}</span>
+          <div class="mt-6 text-gray-600">
+            {{ note.note }}
+          </div>
         </div>
       </div>
-    </div>
+    </client-only>
   </div>
 </template>
 <script>
+const domain = 'https://620496a1c6d8b20017dc3598.mockapi.io'
 export default {
+  scrollToTop: true,
   layout: "default",
   data() {
     return {
-      slug: null
+      slug: null,
+      note: null
     }
   },
   created() {
     this.slug = this.$route.params.slug
-  }
+  },
+  async fetch() {
+    const response = await this.$axios.get(`${domain}/notes/${this.slug}`)
+    this.note = response.data
+    console.log(response.data)
+  },
+  methods: {
+    humanDate(date) {
+      return this.$hDate(date)
+    }
+  },
 }
 </script>

92b05b1e72deb6ee107ab888fb14adeb49ef3580
joesoeph <yusuffazeri00@gmail.com>
2022-04-04 15:54:45 +0700

    Fix html

--- components/NoteListPlain.vue
+++ components/NoteListPlain.vue
@@ -1,19 +1,19 @@
 <template>
-  <NuxtLink :to="slug" class="flex flex-row items-center mb-5">
-    <input type="checkbox" class="flex-shrink-0 w-6 h-6 mr-4 border-gray-300 rounded-md shadow-sm cursor-pointer focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
-    <div class="flex flex-row items-center justify-between flex-1 p-6 bg-white filter drop-shadow-md rounded-xl">
+  <div class="flex flex-row items-center mb-4">
+    <input type="checkbox" class="flex-shrink-0 block w-6 h-6 mr-4 border-gray-300 rounded-md shadow-sm cursor-pointer focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+    <NuxtLink :to="slug"  class="flex flex-row items-center justify-between flex-1 p-6 bg-white filter drop-shadow-md rounded-xl">
       <div>
         <div class="text-lg font-semibold text-gray-700">{{ title }}</div>
         <div class="text-sm font-light text-gray-600">{{ createdAt }}</div>
       </div>
-      <NuxtLink :to="`${slug}/edit`" class="text-blue-500 hover:text-opacity-50" title="Edit this">
+      <NuxtLink :to="`${slug}/edit`" class="block text-blue-500 hover:text-opacity-50" title="Edit this">
         <font-awesome-icon icon="fa-solid fa-pen-to-square" size="lg" />
       </NuxtLink>
-    </div>
-  </NuxtLink>
+    </NuxtLink>
+  </div>
 </template>
 <script>
 export default {
-  props: ['title', 'createdAt', 'slug'],
+  props: ['title', 'createdAt', 'slug']
 }
 </script>

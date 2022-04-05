599fec3190e5e1219cb4eb5407531534dbb03afe
joesoeph <yusuffazeri00@gmail.com>
2022-04-05 08:59:47 +0700

    Centering logo navbar

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,10 +1,10 @@
 <template>
   <header class="flex flex-row items-center justify-between p-4 bg-white filter drop-shadow">
-    <div></div>
-    <div class="text-xl font-semibold text-gray-700">
+    <div class="w-1/3"></div>
+    <div class="flex justify-center w-1/3 text-xl font-semibold text-gray-700">
       <NuxtLink to="/" class="text-gray-700 hover:text-opacity-75" title="Go to home">Taking Note</NuxtLink>
     </div>
-    <div>
+    <div class="flex justify-end w-1/3">
       <NuxtLink to="/profile/edit" class="flex flex-row items-center" title="Go to profile">
         <img src="https://picsum.photos/200" alt="John Doe Picture" class="w-10 h-10 rounded-full">
         <span class="ml-3 text-gray-700 hover:text-opacity-75">Hi, John Doe</span>
